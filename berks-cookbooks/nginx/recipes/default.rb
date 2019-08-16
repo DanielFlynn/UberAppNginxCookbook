4ef4601d9630db0ede9eed15114a1b40a1327334
Daniel Flynn <dflynn@spartaglobal.com>
2019-08-16 10:12:52 +0100

    Created 1st unittest to see if nginx will successfully install. Created a Berksfile and Berksfile.lock

---
+++ berks-cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: nginx
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'nginx'
+
+service 'nginx' do
+  action [:enable, :start]
+end
