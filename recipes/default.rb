189b7ea18aad3158d10e4ec70a0e0abb4f8fcade
Daniel Flynn <dflynn@spartaglobal.com>
2019-08-16 10:04:21 +0100

    Created Recipe to update sources and for package nginx to be installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'nginx'
+
+service 'nginx' do
+  action [:enable, :start]
+end
